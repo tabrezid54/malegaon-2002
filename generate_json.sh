a92a69d96d3a15bdfe07fb57856c77ee41604f72
tabrezid54 <tabrez.shah@opstree.com>
2026-03-26 04:05:21 +0530

    uploaded

---
+++ generate_json.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+output="files.json"
+
+echo "[" > "$output"
+
+files=( *.json )
+count=${#files[@]}
+
+for ((i=0; i<count; i++)); do
+    if [ $i -lt $((count-1)) ]; then
+        echo "  \"${files[$i]}\"," >> "$output"
+    else
+        echo "  \"${files[$i]}\"" >> "$output"
+    fi
+done
+
+echo "]" >> "$output"
+
+echo "Created $output"
